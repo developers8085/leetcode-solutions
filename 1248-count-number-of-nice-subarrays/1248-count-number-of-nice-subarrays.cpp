1d01cb609185f5a4d3088fb389172e39841560ec
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-10-22 13:16:24 +0530

    Time: 765 ms (5.03%), Space: 93.4 MB (19.99%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -6,16 +6,9 @@ public:
         map<int,int> mp;
         int c_sum =0;
         int n = nums.size();
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }else{
-                nums[i]=1;
-            }
-        }
         mp[0]=0;
         for(int i=0;i<n;i++){
+            nums[i] = nums[i]&1;
             c_sum = c_sum +nums[i];
             if(c_sum ==k)
                 ans++;
