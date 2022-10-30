e8bde3eb79fb26d711a9be1643ae0a222c03cfad
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-10-30 23:01:38 +0530

    Added solution - LeetHub

--- Power of 2 - GFG/power-of-2.java
+++ Power of 2 - GFG/power-of-2.java
@@ -37,9 +37,8 @@ class Solution{
     // Function to check if given number n is a power of two.
     public static boolean isPowerofTwo(long n){
         
-        if(n==0)
+        /*if(n==0)
           return false;
-        
         long a = n;
         long b = n-1;
         
@@ -47,7 +46,18 @@ class Solution{
            return true;
         
           return false;
+        */
+        if(n == 0 )
+          return false;
+        if(n == 1)
+         return true;
         
+        while(n>1){
+            if(n%2 !=0)
+              return false;
+              n=n/2;
+        }
+        return true;
     }
     
 }
