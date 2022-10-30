13726022a7c050eddade1da775503c248d73cca3
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-10-30 23:08:49 +0530

    Added solution - LeetHub

--- Anagram - GFG/anagram.cpp
+++ Anagram - GFG/anagram.cpp
@@ -9,6 +9,7 @@ class Solution
     public:
     //Function is to check whether two strings are anagram of each other or not.
     bool isAnagram(string a, string b){
+        /*
         int hs[26]={0};
         
         if(a.size() != b.size())
@@ -26,6 +27,18 @@ class Solution
                 return false;
         }
         return true;
+        */
+        
+        if(a.size() != b.size())
+          return false;
+        
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        
+        if(a==b)
+          return true;
+        else
+         return false;
     }
 
 };
