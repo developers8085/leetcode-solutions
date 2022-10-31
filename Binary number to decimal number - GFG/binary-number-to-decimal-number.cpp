5cf5e67bc7e6e8e64acc9760bf312b1bf00e1263
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-10-31 21:19:53 +0530

    Added solution - LeetHub

---
+++ Binary number to decimal number - GFG/binary-number-to-decimal-number.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int binary_to_decimal(string str) {
+        int ans = 0;
+        int mult = 1;
+        int n = str.size();
+        while(n>0){
+            ans = ans + (str[n-1]-'0')*mult;
+            mult = mult*2;
+            n--;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.binary_to_decimal(str);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
