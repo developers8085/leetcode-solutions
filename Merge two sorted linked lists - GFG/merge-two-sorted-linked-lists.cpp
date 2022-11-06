e99f0a1ccd83b806e44bde36d32410a33b73487c
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-11-06 15:47:58 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+Node *rec(Node *h1,Node *h2){
+    Node *ans = NULL;
+    if(h1 == NULL)
+      return h2;
+    if(h2 == NULL)
+      return h1;
+    
+    if(h1->data <= h2->data){
+        ans = h1;
+        ans->next = sortedMerge(h1->next,h2);
+    }else{
+        ans = h2;
+        ans->next = sortedMerge(h1,h2->next);
+    }
+    return ans;
+}
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* h1, Node* h2)  
+{  
+    return rec(h1,h2);
+}  
