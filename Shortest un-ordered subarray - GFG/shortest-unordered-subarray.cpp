fb5696a62d5fafcc364e8f6be408e9304bbfe697
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-11-15 00:35:36 +0530

    Added solution - LeetHub

---
+++ Shortest un-ordered subarray - GFG/shortest-unordered-subarray.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    int shortestUnorderedSubarray(int a[], int n)
+    {
+        // Cop
+        if(a[0]<a[1]){
+           for(int i=1;i<n-1;i++){
+               if(a[i]>a[i+1])
+                   return 3;
+           }
+       }
+       else{
+           for(int i=1;i<n-1;i++){
+               if(a[i]<a[i+1])
+                   return 3;
+           }
+       }
+       return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) 
+            cin >> a[i];
+        Solution obj;
+        cout << obj.shortestUnorderedSubarray(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
