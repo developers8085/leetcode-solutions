0fc81f108cd1b2b542688b44f09136ec8f4e6640
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-11-23 13:09:27 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -28,24 +28,19 @@ public:
         
         for(int i=0;i<m;i++){
             ans = max(ans,mat[n-1][i]);
-        }
-        /* Recursive approach tried. Did not work
+        } 
+        /*   Recursive approach tried. Did not work timeout
         for(int i=0;i<n;i++){
-            vector<vector<bool>> visited(n);
-            for(int i=0;i<n;i++){
-                visited[i].resize(n,false);
-            }
-            visited[0][i]=true;
-            ans = max(ans,findPath(Matrix,0,i,n,m,visited));
-        } */
+            ans = max(ans,findPath(mat,0,i,n,m));
+        }
+        */
         return ans;
     }
     int maxOf3(int x,int y,int z){
         return max(x,max(y,z));
     }
     
-    int findPath(vector<vector<int>> M,int x,int y,int n,int m,vector<vector<bool>> &visited){
-        
+    int findPath(vector<vector<int>> M,int x,int y,int n,int m){
         if(x>n || y >=m || y<0){
             return 0;
         }
@@ -55,17 +50,14 @@ public:
         int r1 =0;
         int r2=0;
         int r3=0;
-        if(validate(x+1,y,n,m) && visited[x+1][y]==false){
-            visited[x+1][y]=true;
-            r1= findPath(M,x+1,y,n,m,visited);
+        if(validate(x+1,y,n,m) ){
+            r1= findPath(M,x+1,y,n,m);
         }
-        if(validate(x+1,y-1,n,m) &&visited[x+1][y-1]==false){
-            visited[x+1][y-1]=true;
-            r2= findPath(M,x+1,y-1,n,m,visited);
+        if(validate(x+1,y-1,n,m)){
+            r2= findPath(M,x+1,y-1,n,m);
         }
-        if(validate(x+1,y+1,n,m) && visited[x+1][y+1]==false){
-            visited[x+1][y+1]=true;
-            r3=  findPath(M,x+1,y+1,n,m,visited);
+        if(validate(x+1,y+1,n,m)){
+            r3=  findPath(M,x+1,y+1,n,m);
         }
         return M[x][y]+max(r1,max(r2,r3));
     }
