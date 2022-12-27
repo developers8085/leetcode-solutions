85fcb1f9a9877ec1e5ae2f3ac5d19ab5df86444c
jd suresh <112743108+developers8085@users.noreply.github.com>
2022-12-27 23:57:20 +0530

    Added solution - LeetHub

---
+++ Akku and Binary Numbers - GFG/akku-and-binary-numbers.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    int tmp[63*63*63];
+public:
+    void precompute()
+    {
+        
+    }
+    
+    long long solve(long long L, long long R){
+        long long ans = 0;
+        for(int i=0;i<63;i++){
+            for(int j=i+1;j<63;j++){
+                for(int k=j+1;k<63;k++){
+                    long long curr =0L;
+                    curr = curr | 1L<<i;
+                    curr = curr | 1L<<j;
+                    curr = curr | 1L<<k;
+                    if(curr>=L && curr<=R){
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    
+    int leftIndex(long long key){
+        int ans =0;
+        int l =0;
+        int r = 63*63*63-1;
+        while(l<=r){
+            int m = l +(r-l)/2;
+            if(tmp[m]>=key){
+                ans = m;
+                r = m-1;
+            }else{
+                l= m+1;
+            }
+        }
+        return ans;
+    }
+    
+    int rightIndex(long long key){
+        int l =0;
+        int r = 63*63*63-1;
+        int ans =0;
+        while(l<=r){
+            int m = l +(r-l)/2;
+            if(tmp[m]>=key){
+                ans = m;
+                r = m-1;
+            }else{
+                l= m+1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    Solution ob;
+    ob.precompute();
+    cin>>t;
+    while(t--)
+    {
+        long long l,r;
+        cin>>l>>r;
+        cout << ob.solve(l, r) << endl; 
+    }
+    return 0;
+}
+// } Driver Code Ends
