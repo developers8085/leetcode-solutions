fe30f6f7d5ac8173b7ab9d3e8231d2540b10aef4
jd suresh <112743108+developers8085@users.noreply.github.com>
2023-02-12 12:54:40 +0530

    Added solution - LeetHub

---
+++ Subset Sums - GFG/subset-sums.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        // ans.add(0);
+        recursive(arr,0,N-1,ans,0);
+        return ans;
+    }
+    void recursive(ArrayList<Integer> arr,int l,int r,ArrayList<Integer> ans,int sum){
+        if(l>r){
+            ans.add(sum);
+            return ;
+        }
+        recursive(arr,l+1,r,ans,sum+arr.get(l));
+        recursive(arr,l+1,r,ans,sum);
+    }
+}
